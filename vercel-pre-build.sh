ca682a546dc4659d7a2172d5a8d8d4d6cd4a7cbe
Khanh Nguyen <encacap@gmail.com>
2023-05-11 23:34:31 +0700

    chore: prevent vercel deployment on the pull request

--- vercel-pre-build.sh
+++ vercel-pre-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Ignore Vercel build step if the the branch is not main or stg
-if [[ $VERCEL_GIT_COMMIT_REF != "main" && $VERCEL_GIT_COMMIT_REF != "stg" ]]; then
+# Ignore Vercel build step if the the branch is not main or dev
+if [[ $VERCEL_GIT_COMMIT_REF != "main" && $VERCEL_GIT_COMMIT_REF != "dev" ]]; then
   echo "🛑 - Build cancelled"
   exit 0
 fi
