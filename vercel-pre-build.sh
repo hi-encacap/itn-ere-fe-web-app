16ba65d5c8573589841e921db36dab883141ff32
Nguyễn Khắc Khánh <encacap@gmail.com>
2023-06-15 20:30:09 +0700

    fix(ERE-1): use post code to create post URL instead of a fixed code (#21)
    * fix: update product title
    
    * fix: update product layout
    
    * fix: use post code to create post URL instead of a fixed code

--- vercel-pre-build.sh
+++ vercel-pre-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Ignore Vercel build step if the the branch is not main or dev
-if [[ $VERCEL_GIT_COMMIT_REF != "main" && $VERCEL_GIT_COMMIT_REF != "dev" ]]; then
+# Ignore Vercel build step if the the branch is not dev
+if [[ $VERCEL_GIT_COMMIT_REF != "dev" ]]; then
   echo "🛑 - Build cancelled"
   exit 0
 
